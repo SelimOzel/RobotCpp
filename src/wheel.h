4e115606bb3a213f5edf4bd695094aac49318a8c
SelimOzel <sozel@wpi.edu>
2020-05-30 14:12:12 +0300

    Update wheel.h

--- src/wheel.h
+++ src/wheel.h
@@ -8,13 +8,15 @@ class Wheel : public dynamicsystem
 {
 public:
 	// Default constructor with custom initial settings
-	Wheel(std::vector<double>& initialState_IN, 
+	Wheel(
+		std::vector<double>& initialState_IN, 
 		std::vector<double>& initialInput_IN, 
-		std::vector<double>& time_IN) : dynamicsystem(initialState_IN, 
-											initialInput_IN, 
-											time_IN, 
-											NUMBEROFSTATES, 
-											NUMBEROFINPUTS)
+		std::vector<double>& time_IN) : dynamicsystem(
+			initialState_IN, 
+			initialInput_IN, 
+			time_IN, 
+			NUMBEROFSTATES, 
+			NUMBEROFINPUTS)
 	{
 		SetController(&ConstantSpeedController);
 	}
@@ -29,9 +31,9 @@ private:
 	std::vector<double> diff(std::vector<double>& state_IN, std::vector<double>& input_IN)
 	{
 		std::vector<double> s_diff(NUMBEROFSTATES);
-		s_diff[0] = cos(state_IN[2]) * input_IN[0]; // cos(theta) * v
-		s_diff[1] = sin(state_IN[2]) * input_IN[0]; // sin(theta) * v
-		s_diff[2] = input_IN[1];					// w
+		s_diff[0] = cos(state_IN[2]) * input_IN[0]; 	// cos(theta) * v
+		s_diff[1] = sin(state_IN[2]) * input_IN[0]; 	// sin(theta) * v
+		s_diff[2] = input_IN[1];			// w
 		return s_diff;
 	}
 };
